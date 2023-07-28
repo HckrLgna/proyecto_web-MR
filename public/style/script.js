d30003dcede061142c23695df8fe2f31b62bf1a7
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-07-28 10:26:25 -0400

    prepando cambios

--- public/style/script.js
+++ public/style/script.js
@@ -18,18 +18,49 @@ changeTheme = function(){
   var css = document.getElementById("theme");
   var btnTheme = document.getElementById("btn-change-theme");
   var menuMode = document.getElementById("menu-mode");
-
+  var bootMode = document.getElementById("usuarios");
   fileName = css.getAttribute("href");
   if(fileName == "style/dark.css"){
       css.setAttribute("href", "style/light.css");
       btnTheme.innerHTML = '<i class="fas fa-moon"></i><span class="link_name">Night</span>';
       menuMode.innerHTML = '<li><a class="link_name" href="#">Night</a></li>';
+      bootMode.setAttribute("data-bs-theme", "light");
   }else{
       css.setAttribute("href", "style/dark.css");
       btnTheme.innerHTML = '<i class="fas fa-sun"></i><span class="link_name">Light</span>';
       menuMode.innerHTML = '<li><a class="link_name" href="#">Light</a></li>';
+      bootMode.setAttribute("data-bs-theme", "dark");
   }
 }
+var changeThemeAuto = () => {
+    let day = new Date();
+    var hour = day.getHours();
+    var css = document.getElementById("theme");
+    var btnTheme = document.getElementById("btn-change-theme");
+    var menuMode = document.getElementById("menu-mode");
 
+    /**               MODIFICAR ***/
+    var bootMode = document.getElementById("usuarios");
+
+    fileName = css.getAttribute("href");
+    console.log(hour);
+    if (hour>=8 && hour<18) {
+        css.setAttribute("href", "style/light.css");
+        btnTheme.innerHTML = '<i class="fas fa-moon"></i><span class="link_name">Night</span>';
+        menuMode.innerHTML = '<li><a class="link_name" href="#">Night</a></li>';
+
+        /**               MODIFICAR ***/
+        bootMode.setAttribute("data-bs-theme", "light");
+
+    } else {
+        css.setAttribute("href", "style/dark.css");
+        btnTheme.innerHTML = '<i class="fas fa-sun"></i><span class="link_name">Light</span>';
+        menuMode.innerHTML = '<li><a class="link_name" href="#">Light</a></li>';
+
+        /**               MODIFICAR ***/
+        bootMode.setAttribute("data-bs-theme", "dark");
+    }
+}
 let btnTheme = document.getElementById("btn-change-theme");
 btnTheme.addEventListener("click", changeTheme);
+changeThemeAuto()
